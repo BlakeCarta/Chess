66b7460655b06df3d076f759a5afbb824fb29d52
BlakeCarta <175656909+BlakeCarta@users.noreply.github.com>
2025-05-06 20:33:27 -0500

    Added #print_board

--- main.rb
+++ main.rb
@@ -1,19 +1,7 @@
 # File to call that will run a basic game
 
-# Unicode chess symbols, more info here: https://en.wikipedia.org/wiki/Chess_symbols_in_Unicode
-WHITE_KING = '♔'
-WHITE_QUEEN ='♕'
-WHITE_ROOK = '♖'
-WHITE_BISHOP = '♗'
-WHITE_KNIGHT = '♘'
-WHITE_PAWN = '♙'
+require './lib/board.rb'
 
-BLACK_KING = '♚'
-BLACK_QUEEN = '♛'
-BLACK_ROOK = '♜'
-BLACK_BISHOP = '♝'
-BLACK_KNIGHT = '♞'
-BLACK_PAWN = '♟'
-#puts square.encode('utf-8')
-#puts queen
-
+example = ChessBoard.new
+example.setup_board
+example.print_board
