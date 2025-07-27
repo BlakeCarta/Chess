693e5c72f3dac40eeba76b55107207e996e925e8
BlakeCarta <175656909+BlakeCarta@users.noreply.github.com>
2025-07-27 16:36:13 -0500

    Rework custom matchers to be more reliable and expanded tests

--- spec/custom_matcher_spec.rb
+++ spec/custom_matcher_spec.rb
@@ -2,19 +2,133 @@ require './lib/custom_data.rb'
 #require './spec/spec_custom_data_helper.rb'
 
 describe CustomData do
-  context '#equivalent_all_items_of' do
+  context '#have_equal_turns' do
+    subject(:data_one) { described_class.new(38) }
+    subject(:data_two) { described_class.new(38) }
+    it 'has the same turn number' do
+      expect(data_one).to have_equal_turns(data_two)
+    end
+  end
+
+  context '#have_equal_turns' do
+    subject(:data_one) { described_class.new(25) }
+    subject(:data_two) { described_class.new(15) }
+    it 'does not have the same turn numbers' do
+      expect(data_one).not_to have_equal_turns(data_two)
+    end
+  end
+
+  context '#have_equal_white_moves' do
+    subject(:data_one) { described_class.new(38, 'g3') }
+    subject(:data_two) { described_class.new(38, 'g3') }
+    it 'has the same move for the white player' do
+      expect(data_one).to have_equal_white_moves(data_two)
+    end
+  end
+
+  context '#have_equal_white_moves' do
+    subject(:data_one) { described_class.new(38, 'g3') }
+    subject(:data_two) { described_class.new(38, 'g4') }
+    it 'does not have the same move for the white player' do
+      expect(data_one).not_to have_equal_white_moves(data_two)
+    end
+  end
+
+  context '#have_equal_black_moves' do
+    subject(:data_one) { described_class.new(38, 'g3', 'Nxh3') }
+    subject(:data_two) { described_class.new(38, 'g3', 'Nxh3') }
+    it 'has the same move for the black player' do
+      expect(data_one).to have_equal_black_moves(data_two)
+    end
+  end
+
+  context '#have_equal_black_moves' do
+    subject(:data_one) { described_class.new(38, 'g3', 'Nxh3') }
+    subject(:data_two) { described_class.new(38, 'g3', 'Bxh3') }
+    it 'does not have the same move for the black player' do
+      expect(data_one).not_to have_equal_black_moves(data_two)
+    end
+  end
+
+  context '#have_equal_comments' do
+    subject(:data_one) { described_class.new(3, 'Bb5', 'a6', 'This opening is called the Ruy Lopez.') }
+    subject(:data_two) { described_class.new(3, 'Bb5', 'a6', 'This opening is called the Ruy Lopez.') }
+    it 'has the same comment' do
+      expect(data_one).to have_equal_comments(data_two)
+    end
+  end
+
+  context '#have_equal_comments' do
+    subject(:data_one) { described_class.new(3, 'Bb5', 'a6', 'This opening is called the Ruy Lopez.') }
+    subject(:data_two) { described_class.new(3, 'Bb5', 'a6', 'This opening is called a blunder.') }
+    it 'does not have the comment' do
+      expect(data_one).not_to have_equal_comments(data_two)
+    end
+  end
+
+  context '#have_all_items_equal' do
     subject(:data_one) { described_class.new(1, 'e4', 'e5') }
     subject(:data_two) { described_class.new(1, 'e4', 'e5') }
     it 'is true' do
-      expect(data_one).to equivalent_all_items_of(data_two)
+      expect(data_one).to have_all_items_equal(data_two)
     end
   end
   
-  context '#equivalent_all_items_of failure' do
+  context '#have_all_items_equal' do
     subject(:data_one) { described_class.new(1, 'e3', 'e5') } 
-    subject(:data_two) { described_class.new(1, 'e5', 'e5') } #HERE
-    it 'is false' do
-      expect(data_one).not_to equivalent_all_items_of(data_two)
+    subject(:data_two) { described_class.new(1, 'e5', 'e5') }
+    it 'does not have all the same items' do
+      expect(data_one).not_to have_all_items_equal(data_two)
+    end
+
+
+    subject(:example_extracted_turn_data) { described_class.new }
+    it 'correctly detects incorrect input' do
+      example_input = ["1.e4", "b88", "2.Nf3", "Nc6", "3.Bb5", "{This", "opening", "is", "called", "the", "Ruy", "Lopez.}", "3...a6"]
+      turn_1 = described_class.new(1, "e4", "e5")
+      turn_2 = described_class.new(2, "Nf3", "Nc6")
+      turn_3 = described_class.new(3, "Bb5", "a6", "This opening is called the Ruy Lopez.")
+
+      turn_1_correct = described_class.new(1, "e82", "e5")
+
+      actual_turn_data = [nil, turn_1, turn_2, turn_3]
+      expected_turn_data = [nil, turn_1_correct, turn_2, turn_3]
+      expect(actual_turn_data).not_to have_array_of_all_items_equal(expected_turn_data)
+    end
+
+
+    subject(:example_extracted_turn_data) { described_class.new }
+    it 'correctly reads the correct input' do
+      example_input = ["11.c4", "c6", "12.cxb5", "axb5", "13.Nc3", "Bb7", "14.Bg5", "b4", "15.Nb1", "h6", "16.Bh4", "c5", "17.dxe5"]
+      turn_11 = described_class.new(11, "c4", "c6")
+      turn_12 = described_class.new(12, "cxb5", "axb5")
+      turn_13 = described_class.new(13, "Nc3", "Bb7")
+      turn_14 = described_class.new(14, "Bg5", "b4")
+      turn_15 = described_class.new(15, "Nb1", "h6")
+      turn_16 = described_class.new(16, "Bh4", "c5")
+      turn_17 = described_class.new(17, "dxe5")
+
+      actual_turn_data = [nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, turn_11, turn_12, turn_13, turn_14, turn_15, turn_16, turn_17]
+      expected_turn_data = [nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, turn_11, turn_12, turn_13, turn_14, turn_15, turn_16, turn_17]
+      expect(actual_turn_data).to have_array_of_all_items_equal(expected_turn_data)
+    end
+
+    subject(:example_extracted_turn_data) { described_class.new }
+    it 'correctly identifies a mismatch' do
+      example_input = ["11.c4", "c6", "12.cxb5", "axb5", "13.Nc3", "Bb7", "14.Bg5", "b4", "15.Nb1", "h6", "16.Bh4", "c5", "17.dxe5"]
+      turn_11 = described_class.new(11, "c4", "c6")
+      turn_12 = described_class.new(12, "cxb5", "axb5")
+      turn_13 = described_class.new(13, "Nc3", "Bb7")
+      turn_14 = described_class.new(14, "Bg5", "b4")
+      turn_15 = described_class.new(15, "Nb1", "h6")
+      turn_16 = described_class.new(16, "Bh4", "c5")
+      turn_17 = described_class.new(17, "dxe5")
+
+      wrong_turn_17 = described_class.new(17, "dxe5", "c6")
+
+      actual_turn_data = [nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, turn_11, turn_12, turn_13, turn_14, turn_15, turn_16, wrong_turn_17]
+      expected_turn_data = [nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, turn_11, turn_12, turn_13, turn_14, turn_15, turn_16, turn_17]
+      expect(actual_turn_data).not_to have_array_of_all_items_equal(expected_turn_data)
     end
   end
 end
--- spec/support/spec_custom_data_helper.rb
+++ spec/support/spec_custom_data_helper.rb
@@ -1,126 +1,152 @@
-require 'rspec/expectations'
+module CustomDataHelper
+  require 'rspec/expectations'
 
-RSpec::Matchers.define :equivalent_turn_of do |expected|
-  match do |actual|
-    actual.turn_number == expected.turn_number
-  end
-
-  failure_message do |actual|
-    "Expected Turn number #{expected.turn_number} to be equal to #{actual.turn_number}"
-  end
-
-  failure_message_when_negated do |actual|
-    "Expected Turn number #{expected.turn_number} to not be equal to #{actual.turn_number}"
-  end
-end
-
-RSpec::Matchers.define :equivalent_white_move_of do |expected|
-  match do |actual|
-    expected.white_move == actual.white_move
-  end
-
-  failure_message do |actual|
-    "Expected white move #{expected.white_move} to be equal to #{actual.white_move}"
-  end
-
-  failure_message_when_negated do |actual|
-    "Expected white move #{expected.white_move} to not be equal to #{actual.white_move}"
-  end
-end
-
-RSpec::Matchers.define :equivalent_black_move_of do |expected|
-  match do |actual|
-    expected.black_move == actual.black_move
-  end
-
-  failure_message do |actual|
-    "Expected black move #{expected.black_move} to be equal to #{actual.black_move}"
-  end
-
-  failure_message_when_negated do |actual|
-    "Expected black move #{expected.black_move} to not be equal to #{actual.black_move}"
-  end
-end
-
-RSpec::Matchers.define :equivalent_comment_of do |expected|
-  match do |actual|
-    expected.comment == actual.comment
-  end
-
-  failure_message do |actual|
-    "Expected the comment #{expected.comment} to be equal to #{actual.comment}"
-  end
-
-  failure_message_when_negated do |actual|
-    "Expected the comment #{expected.comment} to not be equal to #{actual.comment}"
-  end
-end
-
-RSpec::Matchers.define :equivalent_all_items_of do |expected|
-  match do |actual|
-    expected.all_items == actual.all_items
-  end
-
-  failure_message do |actual|
-    "Expected Turn number #{expected.turn_number} to be equal to #{actual.turn_number},
-    Expected white move #{expected.white_move} to be equal to #{actual.white_move},
-    Expected black move #{expected.black_move} to be equal to #{actual.black_move},
-    Expected the comment #{expected.comment} to be equal to #{actual.comment}"
-  end
-
-  failure_message_when_negated do |actual|
-    "Expected Turn number #{expected.turn_number} to not be equal to #{actual.turn_number},
-    Expected white move #{expected.white_move} to not be equal to #{actual.white_move},
-    Expected black move #{expected.black_move} to not be equal to #{actual.black_move},
-    Expected the comment #{expected.comment} to not be equal to #{actual.comment}"
-  end
-
-  description do
-    "Checks @all_items of CustomData object which contains a copy of all data for easy comparison"
-  end
-end
-
-RSpec::Matchers.define :array_equivalent_all_items_of do |expected|
-  match do |actual|
-    temp_list = []
-    actual.each_with_index do |each, index|
-      if each.nil? && expected[index].nil?
-        temp_list << true
-      elsif !each.nil? && expected[index].nil? || each.nil? && !expected[index].nil?
-        temp_list << false
-      else
-        temp_list << each.all_items == expected[index].all_items
-      end
+  RSpec::Matchers.define :have_equal_turns do |expected|
+    match do |actual|
+      actual.turn_number == expected.turn_number
     end
-    temp_list.all?
-  end
 
-  failure_message do |actual|
-    actual.each_with_index do |each, index|
-      if (!each.nil? && !actual.nil?) && each.all_items != actual[index].all_items
-        "Expected Turn number #{each.turn_number} to be equal to #{actual[index].turn_number},
-        Expected white move #{each.white_move} to be equal to #{actual[index].white_move},
-        Expected black move #{each.black_move} to be equal to #{actual[index].black_move},
-        Expected the comment #{each.comment} to be equal to #{actual[index].comment}"
-        break
-      end
+    failure_message do |actual|
+      "Expected Turn number #{expected.turn_number} to be equal to #{actual.turn_number}"
+    end
+
+    failure_message_when_negated do |actual|
+      "Expected Turn number #{expected.turn_number} to not be equal to #{actual.turn_number}"
+    end
+
+    description do
+      'Compares turn numbers.'
     end
   end
 
+  RSpec::Matchers.define :have_equal_white_moves do |expected|
+    match do |actual|
+      expected.white_move == actual.white_move
+    end
 
-  failure_message_when_negated do |actual|
-    actual.each_with_index do |each, index|
-      if each.all_items == actual[index].all_items
-        "Expected Turn number #{each.turn_number} to not be equal to #{actual[index].turn_number},
-        Expected white move #{each.white_move} to not be equal to #{actual[index].white_move},
-        Expected black move #{each.black_move} to not be equal to #{actual[index].black_move},
-        Expected the comment #{each.comment} to not be equal to #{actual[index].comment}"
-        break
-      end
+    failure_message do |actual|
+      "Expected white move #{expected.white_move} to be equal to #{actual.white_move}"
+    end
+
+    failure_message_when_negated do |actual|
+      "Expected white move #{expected.white_move} to not be equal to #{actual.white_move}"
+    end
+
+    description do
+      "Compares white player's move."
     end
   end
 
-  description do
-    "Similar to equivalent_all_items_of, but ensures each CustomData object of an arry matches expected"
+  RSpec::Matchers.define :have_equal_black_moves do |expected|
+    match do |actual|
+      expected.black_move == actual.black_move
+    end
+
+    failure_message do |actual|
+      "Expected black move #{expected.black_move} to be equal to #{actual.black_move}"
+    end
+
+    failure_message_when_negated do |actual|
+      "Expected black move #{expected.black_move} to not be equal to #{actual.black_move}"
+    end
+
+    description do
+      "Compares black player's move."
+    end
+  end
+
+  RSpec::Matchers.define :have_equal_comments do |expected|
+    match do |actual|
+      expected.comment == actual.comment
+    end
+
+    failure_message do |actual|
+      "Expected the comment #{expected.comment} to be equal to #{actual.comment}"
+    end
+
+    failure_message_when_negated do |actual|
+      "Expected the comment #{expected.comment} to not be equal to #{actual.comment}"
+    end
+
+    description do
+      'Compares commentary associated with the turn.'
+    end
+  end
+
+  RSpec::Matchers.define :have_all_items_equal do |expected|
+    match do |actual|
+      expected.all_items == actual.all_items
+    end
+
+    failure_message do |actual|
+      failure_message = "Expected Turn number #{expected.turn_number} to be equal to #{actual.turn_number}"
+      failure_message += "Expected white move #{expected.white_move} to be equal to #{actual.white_move}"
+      failure_message += "Expected black move #{expected.black_move} to be equal to #{actual.black_move}"
+      failure_message + "Expected the comment #{expected.comment} to be equal to #{actual.comment}"
+    end
+
+    failure_message_when_negated do |actual|
+      failure_message = "Expected Turn number #{expected.turn_number} to not be equal to #{actual.turn_number}"
+      failure_message += "Expected white move #{expected.white_move} to not be equal to #{actual.white_move}"
+      failure_message += "Expected black move #{expected.black_move} to not be equal to #{actual.black_move}"
+      failure_message + "Expected the comment #{expected.comment} to not be equal to #{actual.comment}"
+    end
+
+    description do
+      'Checks @all_items of CustomData object, which contains a copy of all data for easy comparison.'
+    end
+  end
+
+  RSpec::Matchers.define :have_array_of_all_items_equal do |expected|
+    match do |actual|
+      temp_list = []
+      actual.each_with_index do |each, index|
+        expected_index = expected[index]
+        temp_list = if each.nil? && expected_index.nil?
+                      temp_list << true
+                    elsif !each.nil? && expected_index.nil? || each.nil? && !expected_index.nil?
+                      temp_list << false
+                    else
+                      temp_list << (each.all_items == expected_index.all_items)
+                    end
+      end
+      temp_list.all?
+    end
+
+
+
+    failure_message do |actual|
+      failure_message = ""
+      actual.each_with_index do |actual_turn_data, index|
+        expected_index = expected[index]
+        if (actual_turn_data.nil? != expected_index.nil?) || (actual_turn_data&.all_items != expected_index&.all_items)
+          failure_message = "Expected Turn number #{expected_index.turn_number} to be equal to #{actual_turn_data.turn_number}\n"
+          failure_message += "Expected white move #{expected_index.white_move} to be equal to #{actual_turn_data.white_move}\n"
+          failure_message += "Expected black move #{expected_index.black_move} to be equal to #{actual_turn_data.black_move}\n"
+          failure_message += "Expected the comment #{expected_index.comment} to be equal to #{actual_turn_data.comment}\n"
+          break
+        end
+      end
+      failure_message
+    end
+
+    failure_message_when_negated do |actual|
+      failure_message = ""
+      actual.each_with_index do |actual_turn_data, index|
+        expected_index = expected[index]
+        if (actual_turn_data.nil? != expected_index.nil?) || (actual_turn_data&.all_items == expected_index&.all_items)
+          failure_message = "Expected Turn number #{expected_index.turn_number} to not be equal to #{actual_turn_data.turn_number}\n"
+          failure_message += "Expected white move #{expected_index.white_move} to not be equal to #{actual_turn_data.white_move}\n"
+          failure_message += "Expected black move #{expected_index.black_move} to not be equal to #{actual_turn_data.black_move}\n"
+          failure_message += "Expected the comment #{expected_index.comment} to not be equal to #{actual_turn_data.comment}\n"
+          break
+        end
+      end
+      failure_message
+    end
+
+    description do
+      'Compares arrays, using the @all_items property to ensure each CustomData object of each array matches the corresponding object.'
+    end
   end
 end
