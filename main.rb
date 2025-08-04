985bc259e4d66fa183c59a6e4a9fdfbfa553451c
BlakeCarta <175656909+BlakeCarta@users.noreply.github.com>
2025-08-04 17:16:31 -0500

    Added basic support for showing current functions

--- main.rb
+++ main.rb
@@ -1,7 +1,32 @@
 # File to call that will run a basic game
+class Game
+  require './lib/board.rb'
+  require './lib/pgn_reader.rb'
+  def initialize
+  end
 
-require './lib/board.rb'
+  def show_example_outputs
+    @example = ChessBoard.new
+    @example.setup_board
+    @example.print_board
+    setup_example_reader
+    puts "Example reader output: "
+    @example_reader.get_all(@example_file).each do |each|
+      next if each.nil?
+      message = "Turn number #{each.turn_number} "
+      message += "White move #{each.white_move} "
+      message += "Black move #{each.black_move} "
+      message += "The comment #{each.comment}"
+      puts message
+    end
+  end
 
-example = ChessBoard.new
-example.setup_board
-example.print_board
+  def setup_example_reader
+    @example_reader = PgnReader.new
+    @example_file = @example_reader.read_example_file
+  end
+
+end
+
+new_game = Game.new
+new_game.show_example_outputs
