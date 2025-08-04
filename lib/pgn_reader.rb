d069d0757f74bce587fed996ae6a9bc7c329e031
BlakeCarta <175656909+BlakeCarta@users.noreply.github.com>
2025-08-04 11:38:01 -0500

    Rewrite of extract_turn_data

--- lib/pgn_reader.rb
+++ lib/pgn_reader.rb
@@ -1,4 +1,4 @@
-require './lib/custom_data'
+require './lib/custom_data'.freeze
 # Reads Portable Game Notation files (.txt) and converts them into Custom Data format
 class PgnReader
   def initialize(name = nil)
@@ -31,107 +31,64 @@ class PgnReader
   end
 
   def extract_turn_data(line)
-    is_commment = false
     collected_comment = []
-    # export variable
-    collected_turns = [] 
-    current_turn_num = 0
-    current_turn = nil
-    white_move = nil
-    black_move = nil
+    collected_turns = []
+    current_turn = 0
+    white_move, black_move = nil
 
-    # Hangle multi line input in a single string
-    # TODO: Clean this up later if possible
-    line = line.join(' ').split(' ').flatten
-
-
-    line.each do |item| #DEBUG THIS FURTHER LIHE BIG SPACE
-      # if Turn start (digit with dot)
-      if item.match(/\d+\./) && is_commment == false
-        if !current_turn.nil?
-          collected_turns[current_turn_num] = current_turn
-        end
-        # handle double digits
-        if item.match(/\d+\d+\./)
-          current_turn_num = item[0..1].to_i
-        else
-          current_turn_num = item[0].to_i
-        end
-        # white move = '.' this could be improved?
-        if item.count('.') == 1
-          if item[2..-1].match(/\./)
-            white_move = item[3..-1]
-          else
-            white_move = item[2..-1]
-          end
-          # black move = '...'
-        elsif item.count('.') == 3 || item.count('.') < 1 
-          black_move = item[4..-1]
-        end
+    line.each do |item|
+      # if this is a special character, like the end of match notation ensure currently stored data is writtent to array
+      if special_characters?(item)
+        collected_turns[current_turn] = CustomData.new(current_turn, white_move, black_move, collected_comment)
+        next
       end
+      # get and set turn number if that is in the current item
+      current_turn = set_turn_number(item, current_turn)
 
-      # if white move added and black isnt, but guard against readding white to black
-      if !white_move.nil? && !item.match(/\d+\./) && !item.match('{') && is_commment == false
-        black_move = item
-      end
+      # get and set white move if that is current item
+      white_move = set_white_move(item, white_move)
 
-      # if comment ( { or } )
-      if item.match('{') && is_commment == false
-        is_commment = true
-        #collected_comment = []
-        collected_comment << item[1..-1]
-      elsif item.match('}')
-        collected_comment << item[0...-1]
-        collected_comment = collected_comment.join(' ')
-        is_commment = false
-      elsif is_commment == true
-        collected_comment << item
-      end
-      if !white_move.nil? && !black_move.nil? 
-        if collected_comment == []
-          collected_comment = nil
-        end
-        collected_turns[current_turn_num] = CustomData.new(current_turn_num, white_move, black_move, collected_comment)
-        collected_comment = []
-        white_move = nil
-        black_move = nil
-      end
+      # get and set black move if that is the current item
+      black_move = set_black_move(item, black_move, collected_comment)
+
+      # get and set the comment if that is the current item
+      collected_comment = set_comment(item, collected_comment)
+
+      # if both moves not set, don't write to array yet
+      next if white_move.nil? || black_move.nil?
+
+      # if both moves set, add them into the array and reset moves
+      collected_turns[current_turn] = CustomData.new(current_turn, white_move, black_move, collected_comment)
+      white_move, black_move = nil
     end
     @collected_turns = collected_turns
   end
 
-  def get_all(lines = nil)
-    if lines.nil?
-      @collected_turns
-    else
-      extract_turn_data(lines)
-      @collected_turns.each { |each| each.nil? ? next : each.all_items }
-      @collected_turns
-    end
+  # Handle multi line input in a single string
+  def cleanup_lines(lines)
+    lines.join(' ').split
   end
 
+  def get_all(lines = nil)
+    # if given lines, extract them to @collected_turns, otherwise just get the already created list
+    extract_turn_data(cleanup_lines(lines)) unless lines.nil?
+    @collected_turns
+  end
 
-  def get_headers(lines = nil) # this, then move spec helpers? then parse moves?
-    if lines.nil?
-      lines = @full_text
-    end
+  def get_headers(lines = nil)
+    lines = @full_text if lines.nil?
+
     lines.each do |line|
-      if line.nil?
-        next
-      end
+      next if line.nil? || line[0] != '['
 
-      if line[0] == '['
-        line = line.delete('[')
-        line = line.delete(']')
-        line = line.delete('\\"') # found on stack overflow, said may have unintended consequences
-        #split line into chunks
-        line = line.chomp.split(' ')
+      line = line.delete('[]\"')
+      # Split line into chunks
+      line = line.chomp.split(' ')
 
-        line_key = line[0].to_sym
-        line_data = line[1..-1].join(' ')
+      line_key = line[0].to_sym
+      line_data = line[1..].join(' ')
 
-        set_header(line_key, line_data)
-      end
+      set_header(line_key, line_data)
     end
     @headers
   end
@@ -140,4 +97,58 @@ class PgnReader
     @headers[key] = data
   end
 
+  def special_characters?(text)
+    # End of match is a tie
+    if text.match('1/2-1/2')
+      @headers[:result] = text
+      return true
+    end
+    false
+  end
+
+  # if turn start (digit with dot)
+  def set_turn_number(text, current_turn)
+    # double digits
+    if text.match(/\d+\d+\./)
+      current_turn = text[0..1].to_i
+    # single digits
+    elsif text.match(/\d+\./)
+      current_turn = text[0].to_i
+    end
+
+    current_turn
+  end
+
+  def set_white_move(text, white_move)
+    # white move = '.'
+    if text.count('.') == 1 && text.count('}').zero?
+      white_move = text[2..].match(/\./) ? text[3..] : text[2..]
+    end
+    white_move
+  end
+
+  def set_black_move(text, black_move, collected_comment)
+    # black move = '...'
+    if text.count('.') == 3 || text.count('.') > 1
+      black_move = text[4..]
+    # if white move added and black isnt, but guard against reading white to black and against writing comments
+    elsif !text.match(/\d+\./) && !text.match('{') && collected_comment.empty?
+      black_move = text
+    end
+    black_move
+  end
+
+  def set_comment(text, collected_comment)
+    # if comment ( { or } )
+    if text.match('{') && collected_comment.empty?
+      collected_comment << text[1..]
+    elsif text.match('}')
+      collected_comment << text[0...-1]
+      collected_comment = collected_comment.join(' ')
+    elsif !collected_comment.empty? && collected_comment.is_a?(Array)
+      collected_comment << text
+    end
+
+    collected_comment
+  end
 end
