93fc4e347e79e7158485eebefb90b211a0a12f95
BlakeCarta <175656909+BlakeCarta@users.noreply.github.com>
2025-08-04 11:39:41 -0500

    Rework to allow cleaner usage of data object

--- lib/custom_data.rb
+++ lib/custom_data.rb
@@ -3,7 +3,9 @@ class CustomData
     @turn_number = turn_number
     @white_move = white_move
     @black_move = black_move
-    @comment = comment
+    # if comment is '[]' set it to nil, otherwise set to passed value
+    # prevents false test failures due to how current tests are conducted
+    @comment = comment == [] ? nil : comment
     @all_items = []
   end
   attr_accessor :turn_number, :white_move, :black_move, :comment
