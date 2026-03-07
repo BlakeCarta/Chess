0693aa476ad2bdc7a5057535c88ea1b4d0dd648b
BlakeCarta <175656909+BlakeCarta@users.noreply.github.com>
2026-03-07 12:58:26 -0600

    #convert_pos_to_row_col & tests

--- lib/input_manger.rb
+++ lib/input_manger.rb
@@ -1,7 +1,17 @@
 module Input_Manager
   def self.get_input
-    target = [3,0]
-    current = [1,1]
-    return [target, current]
+    target = [3, 0]
+    current = [1, 1]
+    [target, current]
+  end
+
+  def self.convert_posistion_to_row_col(input_text)
+    columns = { 'a': 0, 'b': 1, 'c': 2, 'd': 3, 'e': 4, 'f': 5, 'g': 6, 'h': 7 }
+    column = columns[input_text[0].downcase.to_sym]
+    row = input_text[1].to_i - 1
+
+    return nil if column.nil? || row.nil? || !row.between?(0, 7)
+
+    [row, column]
   end
 end
--- spec/input_manger_spec.rb
+++ spec/input_manger_spec.rb
@@ -1,10 +1,34 @@
-require_relative '../lib/input_manger.rb'
+require_relative '../lib/input_manger'
 describe Input_Manager do
   subject { Input_Manager }
+
   describe '#get_input' do
     it 'returns the default value' do
       target, current = subject.get_input
-      expect([target, current]).to match_array([[3,0], [1,1]])
+      expect([target, current]).to contain_exactly([3, 0], [1, 1])
+    end
+
+    xit '*collect user input*' do
+      # get row, column input 'e4'
+      # convert to internal cordinates
+      # use to select
+      # show moves in similiar output format e5, f4, d3
+      # allow another input to select another piece, select a move
+    end
+  end
+
+  describe '#convert_posistion_to_row_col' do
+    it 'returns 3,4 for e4' do
+      input = 'e4'
+      expected = [3, 4]
+
+      expect(subject.convert_posistion_to_row_col(input)).to eq(expected)
+    end
+
+    it 'returns nil for invalid input' do
+      input = 'el'
+
+      expect(subject.convert_posistion_to_row_col(input)).to be_nil
     end
   end
 end
